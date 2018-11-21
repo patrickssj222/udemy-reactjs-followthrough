63966de99401814e8ecc01319ae3b88e77407144
Patrick Hao <patrick@arblabs.com>
2018-11-21 16:33:26 -0500

    section 6 completed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import classes from './App.css';
 import Person from "./Person/Person"
-
+import ErrorBoundary from "./ErrorBoundary/ErrorBoundary"
 class App extends Component {
   state = {
     persons: [
@@ -48,13 +48,13 @@ class App extends Component {
       person = (
         <div>
           {this.state.persons.map((person, index)=>{
-            return <Person
+            return <ErrorBoundary><Person
               click = {()=>this.deletePersonHandler(index)}
               change = {(event)=>this.nameChangeHandler(event, person.id)}
               name={person.name}
               age={person.age}
               key={person.id}
-            />
+            /></ErrorBoundary>
           })}
         </div>
       );
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react';
 import classes from './Person.css'
 const person = (props) =>{
+
+  const rnd = Math.random();
+  if ( rnd > 0.7 ){
+    throw new Error('Something went wrong');
+  }
+
   return (
     <div className={classes.Person}>
       <p onClick={props.click}>Im {props.name}, and I am {props.age}</p>
